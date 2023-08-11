8add9e7890748196a4cd29106abdf5f42aec77a3
conley521 <35540949+conley521@users.noreply.github.com>
2023-08-10 23:05:42 -0400

    Separating the main dataframe into various data frames based on filtering requirements

--- RobinhoodJournalScript.py
+++ RobinhoodJournalScript.py
@@ -9,6 +9,18 @@ file_path = 'robinhood.csv'
 extracted_columns = ['Activity Date', 'Instrument',
                      'Description', 'Trans Code', 'Quantity', 'Price', 'Amount']
 
-df = pd.read_csv(file_path, usecols=extracted_columns)
+# Extract the data
+all_data = pd.read_csv(file_path, usecols=extracted_columns)
 
-print(df)
+
+options_data = all_data[all_data['Trans Code'].isin(
+    ['STO', 'STC', 'OEXP', 'OASGN', 'BTO', 'BTC', 'OEXCS', 'OCA'])]
+shares_data = all_data[all_data['Trans Code'].isin(
+    ['Buy', 'Sell', 'SPL', 'OASGN', 'AFEE', 'SXCH', 'REC', 'CIL'])]
+dividend_data = all_data[all_data['Trans Code'].isin(['CDIV'])]
+interest_data = all_data[all_data['Trans Code'].isin(['INT'])]
+gold_data = all_data[all_data['Trans Code'].isin(['GOLD'])]
+stock_lending_data = all_data[all_data['Trans Code'].isin(['SLIP'])]
+ach_data = all_data[all_data['Trans Code'].isin(['ACH'])]
+misc_data = all_data[all_data['Trans Code'].isin(['MISC', 'ROC', 'DTAX'])]
+print(ach_data)
